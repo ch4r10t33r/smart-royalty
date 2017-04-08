b4d12d9e24f65e26ab3731008d7ded743edefcad
Parthasarathy Ramanujam <zincoshine>
2017-04-08 11:20:01 +0530

    Changes to the Dapp. Added modules for signing an artefact.

--- mydapp/routes/index.js
+++ mydapp/routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var sha512sum = require('sha512sum');
-var multer  = require('multer')
-var upload = multer({ dest: 'uploads/' })
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
+var web3 = require('web3');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -15,13 +16,16 @@ router.post('/', upload.single('artefact'), function(req,res,ext) {
   console.log('%s file uploaded',req.file.filename);
   res.redirect('/?success=true');
   */
+  var hashString = '';
   console.log("%s uploaded as %s at %s.",req.file.originalname,req.file.filename,req.file.path);
 
-  sha512sum.fromFile(req.file.path,function(err,hash) {
+  sha512sum.fromFile(req.file.path,{sep: '|'},function(err,hash) {
     if (err) throw err;
-    console.log(hash);
+    hashString = hash.substring(0,hash.indexOf('|'));
+    console.log('Generated hash: ' + hashString);
+    res.end('<pre><h3>Artefact signing successful. Signature generated: ' + hashString+'</h3></pre>');
   });
-  res.write('successful');
+  console.log('Processing done.')
 });
 
 module.exports = router;
